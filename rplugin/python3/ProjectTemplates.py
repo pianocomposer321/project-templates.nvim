1f85c73fd1c5d655919ff16fc97d1ba2a719a007
= <=>
2020-05-12 18:11:10 -0500

    Fixed SaveAsTemplate

--- rplugin/python3/ProjectTemplates.py
+++ rplugin/python3/ProjectTemplates.py
@@ -99,7 +99,7 @@ class ProjectTemplate(object):
         pwd = self.vim.eval('getcwd()')
         template_name = self.vim.eval('input("Enter the name of the new template> ")')
 
-        shutil.copytree(pwd, self.projectDir + "\\" + template_name)
+        shutil.copytree(pwd, self.projectDir + os.sep() + template_name)
 
         self.vim.command("redraw | echo")
         self.vim.command(f"echo 'Template {template_name} Created.'")
